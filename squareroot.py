7d90cc60947d618169dee46ab150dd235659f086
yimak <123771666+yimak@users.noreply.github.com>
2023-04-12 16:42:54 +0100

    week 06

--- squareroot.py
+++ squareroot.py
@@ -1,4 +1,4 @@
-N=14.5
+N=float(input('Please enter a positive number: '))
 approx=1
 sqrt=1+1
 while (approx!=sqrt):
@@ -6,4 +6,4 @@ while (approx!=sqrt):
     sqrt=0.5*(approx+N/approx)
     if approx==sqrt:
         continue
-print(sqrt)
+print('The square root of 14.5 is approx ', sqrt)
