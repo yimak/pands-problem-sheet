a86a05ea6003a202e2204c0e2567da627a779532
yimak <issam.chaaraddine@hotmail.com>
2023-04-24 11:37:27 +0100

    week 08

--- plottas.py
+++ plottas.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 data = np.random.normal(loc=5, scale=2, size=1000)
 plt.hist(data,edgecolor='white')
-#now we will create a plot of the function  h(x)=x3 in the range [0, 10]
+#now we will create a plot of the function  h(x)=x3 in the range [0, 10],
 xpoint=np.random.normal(range(0,10))
 ypoint= xpoint ** 3
-plt.title("My Chart")
+plt.title("My Chart") 
 plt.plot(xpoint,ypoint,color='red', label='plotfunction')
 plt.legend()
 plt.show()
