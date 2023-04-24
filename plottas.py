33dcdcdb7994cc28421e340d39cca4427f5447fc
yimak <issam.chaaraddine@hotmail.com>
2023-04-24 11:02:43 +0100

    week 08

---
+++ plottas.py
@@ -0,0 +1,15 @@
+#create a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.random.normal(loc=5, scale=2, size=1000)
+plt.hist(data,edgecolor='white')
+#now we will create a plot of the function  h(x)=x3 in the range [0, 10]
+xpoint=np.random.normal(range(0,10))
+ypoint= xpoint ** 3
+plt.plot(xpoint,ypoint,color='red', label='plotfunction')
+plt.legend()
+plt.show()
+
+
+
+
