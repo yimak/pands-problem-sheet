773d44e46369b079eba15e9413917e65640eb066
yimak <issam.chaaraddine@hotmail.com>
2023-04-25 00:25:27 +0100

    week 04

--- collitz.py
+++ collitz.py
@@ -1,7 +1,7 @@
 #Author: Issam Chaaraddine
 #write a program that asks the user to input any positive integer and outputs the successive values of the following calculation.
 #At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
-number = 10
+number=int(input('Please enter a positive integer:'))
 while number > 1:
    if(number%2) == 0:
       number = number/2
