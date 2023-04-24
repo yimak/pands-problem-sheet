157bd23d6a4f9dd2e0cee6d12dcb24cfde849b39
yimak <issam.chaaraddine@hotmail.com>
2023-04-25 00:15:33 +0100

    week 04

--- collitz.py
+++ collitz.py
@@ -1,11 +1,11 @@
 #Author: Issam Chaaraddine
 #write a program that asks the user to input any positive integer and outputs the successive values of the following calculation.
 #At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
-count = 10
-while count > 1:
-   if(count%2) == 0:
-      count = count/2
+number = 10
+while number > 1:
+   if(number%2) == 0:
+      number = number/2
    else:
-      count = count*3+1
-   print(count) 
-print ("at the end the value is ", count)
+      number = number*3+1
+   print(number, end=' ')
+print ("at the end the value is ",number)
