374191ba6ea43b60809102e828bb28783fc7469d
yimak <issam.chaaraddine@hotmail.com>
2023-04-24 22:58:37 +0100

    week 03

--- accounts.py
+++ accounts.py
@@ -1,21 +1,14 @@
 #author: Issam Chaaraddine
 #Write a python program called accounts.py 
-#the program should read in a 10 character account number and outputs the account number with only the last 4 digits
-#replace first 6 digits with 'x'
-#first we will create a string that will read 'please enter a 10 digit number' using a built-in input() function
-a = int(input('Please enter a 10 digit account number: '))
-#we will give the variable 'b' the string value '1234567890'
-b = "1234567890"
-#we will replace the values in string '123456' by 'xxxxxx' using the built-in replace() function
-x = b.replace('123456','xxxxxx')
-print(x)
-#extra, modify the program to deal with account numbers of any length
-#first we will measure the length of variable 'b'
-totalnumbers = len(b)
-#we will create a variable 'numbershidden', it will represent all the numbers except the 4 last digits
+#the program should read in a 10 character account number and outputs the account number with only the last 4 digits, replace the first 6 digits with 'x'
+a = str(input('Please enter a 10 digit account number: '))
+numbersshown1 = a[-4:]
+numbershidden1 = 'X' * 6 + numbersshown1 
+print(numbershidden1)
+
+# Modify the program to deal with account numbers of any length (yes that is a vague requirement, comment your assumptions)
+a = str(input('Please enter the digits of your account number: '))
+totalnumbers = len(a)
 numbershidden = totalnumbers - 4
-#now a variable 'numbersshown'  that will make a shallow copy of the array and will be showing all numbers that are not included in 'numbershidden'
-numbersshown = b[numbershidden:]
-#now we will multiply(*) X by the digits hidden named variable 'numbershidden' and will add(+) the visible digits or 'numbersshown'
+numbersshown = a[numbershidden:]
 print("X" * numbershidden + numbersshown)
-#I remplaced 'x' by 'X' to see the difference when running both programms
